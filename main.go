c522d084dcb50d024d68dad9bc4ca2fc7175b197
WhoisMaiku <115578439+WhoisMaiku@users.noreply.github.com>
2023-08-31 22:49:44 +0100

    Update main.go
    Added handlePostPokemon function to add post functionality to the API. 
    
    Also refactored handleGetPokemon into two separate functions, handleGetAllPokemon, and handleGetPokemonByNumber to increase readability.

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 	"strings"
 
 	_ "modernc.org/sqlite"
@@ -19,8 +20,8 @@ type Pokemon struct {
 
 // Added log.Fatal so that it sends an error if the server crashes
 func main() {
-	http.HandleFunc("/pokemon", handlePokemon)
-	http.HandleFunc("/pokemon/", handlePokemon) // Emulates "/pokemon/{id}" on a framework as this is a limitation of only using net/http
+	http.HandleFunc("/pokemon", handleGetAllPokemon) // Should only be doing GET requests on this route for all pokemon
+	http.HandleFunc("/pokemon/", handlePokemon)      // Emulates "/pokemon/{id}" on a framework as this is a limitation of only using net/http
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
@@ -33,11 +34,19 @@ func WriteJSON(w http.ResponseWriter, status int, v any) error {
 
 // Handles the incoming http requests and routes them depending on the method used.
 func handlePokemon(w http.ResponseWriter, r *http.Request) {
+	// Opens the pokemon database & defers closing until the end of the function
+	db, errs := sql.Open("sqlite", "./pokemon.db")
+	if errs != nil {
+		log.Fatal(errs)
+	}
+	defer db.Close()
+
+	// Switch statement to route the request depending on the method used
 	switch r.Method {
 	case "GET":
-		handleGetPokemon(w, r)
+		handleGetPokemonByNumber(w, r, db)
 	case "POST":
-		w.Write([]byte("This is a post request"))
+		handlePostPokemon(w, r, db)
 	case "PATCH":
 		w.Write([]byte("This is a patch request"))
 	case "DELETE":
@@ -48,10 +57,7 @@ func handlePokemon(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func handleGetPokemon(w http.ResponseWriter, r *http.Request) any {
-	var allPokemon []Pokemon
-	var query string
-
+func handleGetAllPokemon(w http.ResponseWriter, r *http.Request) {
 	// Opens the pokemon database & defers closing until the end of the function
 	db, errs := sql.Open("sqlite", "./pokemon.db")
 	if errs != nil {
@@ -59,20 +65,12 @@ func handleGetPokemon(w http.ResponseWriter, r *http.Request) any {
 	}
 	defer db.Close()
 
-	// If the client added an id to the end of the URL this section extracts the id
-	id := strings.TrimPrefix(r.URL.Path, "/pokemon/")
-
-	// Checks to see if the GET request was just on "/pokemon" If so, it sets query to return all pokemon in the database
-	if r.URL.Path == "/pokemon" || id == "" {
-		query = "SELECT number, name FROM pokemon;"
-	} else {
-		query = fmt.Sprintf("SELECT number, name FROM pokemon WHERE number=%s", id)
-	}
+	allPokemon := []Pokemon{}
 
 	// Runs the query, and then defers closing the query until the end of the function.
-	rows, err := db.Query(query)
+	rows, err := db.Query("SELECT number, name FROM pokemon;")
 	if err != nil {
-		fmt.Println("Error Getting pokemon")
+		fmt.Println("Error getting pokemon")
 		log.Fatal(err)
 	}
 	defer rows.Close()
@@ -94,5 +92,70 @@ func handleGetPokemon(w http.ResponseWriter, r *http.Request) any {
 
 		allPokemon = append(allPokemon, thisPokemon)
 	}
-	return WriteJSON(w, http.StatusOK, allPokemon)
+	WriteJSON(w, http.StatusOK, allPokemon)
+}
+
+func handleGetPokemonByNumber(w http.ResponseWriter, r *http.Request, db *sql.DB) any {
+	// Extracts the id from the URL as a string
+	numString := strings.TrimPrefix(r.URL.Path, "/pokemon/")
+
+	//convert idString to int id and returns bad request if it is not a number
+	num, err := strconv.Atoi(numString)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte("Please enter a valid Number"))
+		return nil
+	}
+
+	// Gets maximum number of pokemon in database
+	var maxNumber int
+	query := db.QueryRow("SELECT MAX(number) FROM pokemon;")
+	err = query.Scan(&maxNumber)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Checks if the id is valid and returns bad request if it is not
+	if num < 1 || num > maxNumber {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte("Please enter a valid Number (Between 1 and " + strconv.Itoa(maxNumber) + ")"))
+		return nil
+	}
+
+	// Runs the query on a single row of the database.
+	row := db.QueryRow("SELECT number, name FROM pokemon WHERE number=?", num)
+
+	var number int
+	var name string
+
+	// Scans the row and places the data into the variable thisPokemon
+	e := row.Scan(&number, &name)
+	if e != nil {
+		log.Fatal(e)
+	}
+
+	thisPokemon := Pokemon{
+		Number: number,
+		Name:   name,
+	}
+	return WriteJSON(w, http.StatusOK, thisPokemon)
+}
+
+func handlePostPokemon(w http.ResponseWriter, r *http.Request, db *sql.DB) any {
+	// Extracts the data from the POST request
+	var pokemon Pokemon
+	err := json.NewDecoder(r.Body).Decode(&pokemon)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Inserts the new pokemon into the database
+	query := fmt.Sprintf("INSERT INTO pokemon VALUES (%d, '%s')", pokemon.Number, pokemon.Name)
+	_, err = db.Exec(query)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Returns http status 200
+	return http.StatusOK
 }
