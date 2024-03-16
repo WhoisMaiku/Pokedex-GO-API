e739bc5e23233a8e91f15a5eac1aee3b64fd2f40
WhoisMaiku <115578439+WhoisMaiku@users.noreply.github.com>
2024-03-16 18:43:27 +0000

    Updated GetAllPokemon & GetPokemonByNumber
    Removed the temporary variables from these handler functions that were created to map the data from the SQL query into the thisPokemon variable, and instead mapped the data directly into the thisPokemon variables in both functions.

--- main.go
+++ main.go
@@ -14,15 +14,17 @@ import (
 
 // struct to create Pokemon objects before encoding into JSON
 type Pokemon struct {
+	Id     int    `json:"id"`
 	Number int    `json:"number"`
 	Name   string `json:"name"`
+	Sprite string `json:"sprite"`
 }
 
 // Added log.Fatal so that it sends an error if the server crashes
 func main() {
 	http.HandleFunc("/pokemon", handleGetAllPokemon) // Should only be doing GET requests on this route for all pokemon
 	http.HandleFunc("/pokemon/", handlePokemon)      // Emulates "/pokemon/{id}" on a framework as this is a limitation of only using net/http
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Fatal(http.ListenAndServe(":3000", nil))
 }
 
 // Encodes data into JSON
@@ -32,10 +34,18 @@ func WriteJSON(w http.ResponseWriter, status int, v any) error {
 	return json.NewEncoder(w).Encode(v)
 }
 
+func enableCors(w *http.ResponseWriter) {
+	// Enables CORS for the frontend to access the API
+	(*w).Header().Set("Access-Control-Allow-Origin", "http://localhost:5173")
+}
+
 // Handles the incoming http requests and routes them depending on the method used.
 func handlePokemon(w http.ResponseWriter, r *http.Request) {
+	// Enables CORS
+	enableCors(&w)
+
 	// Opens the pokemon database & defers closing until the end of the function
-	db, errs := sql.Open("sqlite", "./pokemon.db")
+	db, errs := sql.Open("sqlite", "./all-pokemon.db")
 	if errs != nil {
 		log.Fatal(errs)
 	}
@@ -46,7 +56,7 @@ func handlePokemon(w http.ResponseWriter, r *http.Request) {
 	case "GET":
 		handleGetPokemonByNumber(w, r, db)
 	case "POST":
-		handlePostPokemon(w, r, db)
+		handlePostPokemon(&w, r, db)
 	case "PATCH":
 		w.Write([]byte("This is a patch request"))
 	case "DELETE":
@@ -57,9 +67,13 @@ func handlePokemon(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+// Handles the GET request for all pokemon
 func handleGetAllPokemon(w http.ResponseWriter, r *http.Request) {
+	// Enables CORS
+	enableCors(&w)
+
 	// Opens the pokemon database & defers closing until the end of the function
-	db, errs := sql.Open("sqlite", "./pokemon.db")
+	db, errs := sql.Open("sqlite", "./all-pokemon.db")
 	if errs != nil {
 		log.Fatal(errs)
 	}
@@ -68,7 +82,7 @@ func handleGetAllPokemon(w http.ResponseWriter, r *http.Request) {
 	allPokemon := []Pokemon{}
 
 	// Runs the query, and then defers closing the query until the end of the function.
-	rows, err := db.Query("SELECT number, name FROM pokemon;")
+	rows, err := db.Query("SELECT * FROM pokemon;")
 	if err != nil {
 		fmt.Println("Error getting pokemon")
 		log.Fatal(err)
@@ -77,17 +91,12 @@ func handleGetAllPokemon(w http.ResponseWriter, r *http.Request) {
 
 	// Extracts the data from the query, and places it into a slice of allPokemon
 	for rows.Next() {
-		var number int
-		var name string
 
-		err := rows.Scan(&number, &name)
+		thisPokemon := Pokemon{}
+
+		err := rows.Scan(&thisPokemon.Id, &thisPokemon.Number, &thisPokemon.Name, &thisPokemon.Sprite)
 		if err != nil {
 			log.Fatal(err)
-		}
-
-		thisPokemon := Pokemon{
-			Number: number,
-			Name:   name,
 		}
 
 		allPokemon = append(allPokemon, thisPokemon)
@@ -95,6 +104,7 @@ func handleGetAllPokemon(w http.ResponseWriter, r *http.Request) {
 	WriteJSON(w, http.StatusOK, allPokemon)
 }
 
+// Handles the GET request for a single pokemon
 func handleGetPokemonByNumber(w http.ResponseWriter, r *http.Request, db *sql.DB) any {
 	// Extracts the id from the URL as a string
 	numString := strings.TrimPrefix(r.URL.Path, "/pokemon/")
@@ -123,25 +133,21 @@ func handleGetPokemonByNumber(w http.ResponseWriter, r *http.Request, db *sql.DB
 	}
 
 	// Runs the query on a single row of the database.
-	row := db.QueryRow("SELECT number, name FROM pokemon WHERE number=?", num)
+	row := db.QueryRow("SELECT * FROM pokemon WHERE number=?", num)
 
-	var number int
-	var name string
+	thisPokemon := Pokemon{}
 
 	// Scans the row and places the data into the variable thisPokemon
-	e := row.Scan(&number, &name)
+	e := row.Scan(&thisPokemon.Id, &thisPokemon.Number, &thisPokemon.Name, &thisPokemon.Sprite)
 	if e != nil {
 		log.Fatal(e)
 	}
 
-	thisPokemon := Pokemon{
-		Number: number,
-		Name:   name,
-	}
 	return WriteJSON(w, http.StatusOK, thisPokemon)
 }
 
-func handlePostPokemon(w http.ResponseWriter, r *http.Request, db *sql.DB) any {
+// Handles the POST request for a single pokemon
+func handlePostPokemon(w *http.ResponseWriter, r *http.Request, db *sql.DB) any {
 	// Extracts the data from the POST request
 	var pokemon Pokemon
 	err := json.NewDecoder(r.Body).Decode(&pokemon)
@@ -149,8 +155,17 @@ func handlePostPokemon(w http.ResponseWriter, r *http.Request, db *sql.DB) any {
 		log.Fatal(err)
 	}
 
-	// Inserts the new pokemon into the database
-	query := fmt.Sprintf("INSERT INTO pokemon VALUES (%d, '%s')", pokemon.Number, pokemon.Name)
+	// Checks if the pokemon already exists in the database
+	check := db.QueryRow("SELECT * FROM pokemon WHERE number=?", pokemon.Number)
+	err = check.Scan(&pokemon.Id, &pokemon.Number, &pokemon.Name, &pokemon.Sprite)
+	if err == nil {
+		(*w).WriteHeader(http.StatusBadRequest)
+		(*w).Write([]byte("Pokemon already exists"))
+		return nil
+	}
+
+	// Adds the pokemon to the database
+	query := fmt.Sprintf("INSERT INTO pokemon VALUES (%d, %d, '%s', '%s')", pokemon.Id, pokemon.Number, pokemon.Name, pokemon.Sprite)
 	_, err = db.Exec(query)
 	if err != nil {
 		log.Fatal(err)
